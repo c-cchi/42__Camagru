5566a54c9a5ae5be69567e2aebf0b4fb3db2ad7b
Chia-han CHI <cchi@e3r4p12.42.fr>
2019-11-21 17:06:27 +0100

    index

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+
+    ob_start();
+    function autoloadFunction($class){
+        if (preg_match('/Controller$/', $class))
+            require("Controllers/" . $class . ".php");
+        else
+            require("Models/" . $class . ".php");
+    }
+
+    spl_autoload_register("autoloadFunction");
+    $router = new RouterController();
+    $router->process(array($_SERVER['REQUEST_URI']));
+    $router->renderView();
+        
+    $title = "Camagrue";
+    $content = ob_get_clean();
+    require('template.php');
+
     // opcache_reset(); 
 
     // ob_start();

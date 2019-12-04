d0daf8ddec0b84782995e2318d78403a71febd41
Chia-han CHI <cchi@e3r3p12.42.fr>
2019-12-04 16:50:22 +0100

    add profile page controller

--- Controllers/ProfileController.php
+++ Controllers/ProfileController.php
@@ -1,32 +1,13 @@
 <?php
     class ProfileController extends Controller{
-        function porcess($params){
+        function process($params){
             $this->view = 'profile';
             $this->renderView();
         }
 
-        public function proImg(){
-            session_start();
-            $conn = Connection::getInstance();
+        // public function proImg(){
+        //     session_start();
         
-            $sql = "SELECT * FROM users";
-            $arr = array();
-            $rslt = Connection::getInstance()->runQuery($sql, $arr);
-            if (my_sqli_num_rows($rslt) > 0){
-                while ($row = mysqli_fetch_assoc($rslt)){
-                    $id = $row['id'];
-                    $sqlImg = "SELECT * FROM profileimg WHERE userid ='$id'";
-                    $rsltImg = mysqli_query($conn, $sqlImg);
-                    while($rowImg = mysqli_fetch_assoc($rsltImg)){
-                        echo "<div>";
-                        if ($rowImag['status'] == 0){ // already upload img
-                            echo "<img src='uploads/profile".$id.".jpg?".mt_rand().">"; //mt rand給予一個random數字，這樣在更新圖片的時候browser才不會記住之前的那張照片而沒有更換
-                        }else{
-                            echo "<img src='uploads/profiledefault.jpg'>";
-                        }
-                        echo "</div>";
-                    }
-                }
-            }
-        }
+        //     $promodel = new ProfileModel;
+        // }
     }

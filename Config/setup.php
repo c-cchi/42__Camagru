6a1db6845a93cd1c2b68008de1870e3e811a967e
Chia-han CHI <cchi@e1r7p16.42.fr>
2019-12-15 17:10:25 +0100

    add setup for database

--- Config/setup.php
+++ Config/setup.php
@@ -1,1 +1,30 @@
-    
+<?php
+$con = new mysqli("localhost", "root", "123456");
+if ($con->connect_error)
+    die("Connection failed: " . $con->connect_error . "\n");
+
+$sql = "CREATE DATABASE camagrue";
+if ($con->query($sql) === FALSE)
+    echo "Error creating database: " . $con->error . "\n";
+
+if ($con->query("USE camagrue") === FALSE)
+    echo "Error using database: " . $con->error . "\n";
+
+$sql = "CREATE TABLE users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(400) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    valid_mail int DEFAULT 0,
+    )";
+if ($con->query($sql) === FALSE)
+    echo "Error creating users table: " . $con->error . "\n";
+
+$sql = "CREATE TABLE photos (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    src TEXT NOT NULL,
+    )";
+if ($con->query($sql) === FALSE)    echo "Error creating products table: " . $con->error . "\n";
+
+$con->close();
+?>
--- Controllers/GalleryController.php
+++ Controllers/GalleryController.php
@@ -4,10 +4,6 @@
         public function process($params){
             $this->view = 'gallery';
             $this->renderView();
-            $login = new LoginController;
-            $login->process('login');
-            // if (isset($_SESSION['user'])){
-            // }else{
-            // }
+
         }
     }

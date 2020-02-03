4c1bfeb2de68ca07f8afcd4c127e625e95190cd7
Chia-han CHI <cchi@e1r3p14.42.fr>
2020-02-03 14:38:41 +0100

    take pic

--- js/camera.js
+++ js/camera.js
@@ -1,33 +1,26 @@
 window.addEventListener('load', startCamera());
-
-var video = document.getElementById('player');
+document.getElementById("capture-btn").addEventListener("click", takepicture);
 
 function startCamera(){
     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
             player.srcObject = stream;
-            video.play();
+            // video.play();
         });
     }
 }
+function takepicture() {
+    var camera = document.getElementById("player");
+    var canvas = document.getElementById("canvas").getContext('2d').drawImage(camera, 0, 0, 640 , 480);
+    var data = canvas.toDataURL('image/png');
+    // document.getElementById('photo').setAttribute('src', data);
+}
 
-// navigator.getMedia = ( navigator.getUserMedia ||
-//     navigator.webkitGetUserMedia ||
-//     navigator.mozGetUserMedia ||
-//     navigator.msGetUserMedia);
-
-// navigator.getMedia(
-//     {video: true},
-//     function(stream) {
-//         if (navigator.mozGetUserMedia) {
-//         video.mozSrcObject = stream;
-//         } else {
-//         var vendorURL = window.URL || window.webkitURL;
-//         video.src = vendorURL.createObjectURL(stream);
-//         }
-//         video.srcObject = stream;
-//         video.play();
-//     },  
-//     function(err) {
-//         console.log("An error occured! " + err);
-// });
+// const screenshotButton = document.querySelector('#capture-btn');
+// screenshotButton.onclick  = function() {
+//     // canvas.width = video.videoWidth;
+//     // canvas.height = video.videoHeight;
+//     canvas.getContext('2d').drawImage(player, 0, 0, 640, 480);
+//     // Other browsers will fall back to image/png
+//     img.src = canvas.toDataURL('image/png');
+// };

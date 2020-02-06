32fa92f432b58278fce51596f0d2059b04b19f02
Chia-han CHI <cchi@e1r4p14.42.fr>
2020-02-06 15:58:07 +0100

    modify notification of error pwdrpt

--- Controllers/NewuserController.php
+++ Controllers/NewuserController.php
@@ -3,6 +3,7 @@
         protected $username;
         protected $email;
         protected $pwd;
+        protected $pwdrepeat;
 
         public function __construct(){
             if (isset($_POST['uid'])){
@@ -11,29 +12,36 @@
                 $this->email = $_POST['email'];
             }if (isset($_POST['pwd'])){
                 $this->pwd = $_POST['pwd'];
+            }if (isset($_POST['pwd-repeat'])){
+                $this->pwdrepeat = $_POST['pwd-repeat'];
             }
         }
 
         public function process($newuser){
-            $this->view = 'newuser';
             
-            if (!isset($_POST['signup'])){
+            if (isset($_SESSION['user'])){
+                $this->view = 'Logout';
                 $this->renderView();
-            }else if(empty($this->username) || empty($this->email) || empty($this->pwd)){
-                header("Location: newuser?error=emptyfields&uid=".$this->username."&email=".$this->email); 
-                exit();
-            }else if(!filter_var($this->email, FILTER_VALIDATE_EMAIL) === true){
-                header("Location: newuser?error=invalidemail&uid=".$this->username);
-                exit();
-            }else if (!preg_match("/^[a-zA-Z0-9]*$/", $this->username)){
-                header("Location: newuser?error=invaliduid&email=".$email);
-                exit();
             }else{
-                $rslt = $this->addUser();
-                if ($rslt == TRUE){
-                    $this->redirect("gallery");
+                $this->view = 'newuser';
+                if (!isset($_POST['signup'])){
+                    $this->renderView();
+                }else if(empty($this->username) || empty($this->email) || empty($this->pwd)){
+                    header("Location: newuser?error=emptyfields&uid=".$this->username."&email=".$this->email); 
+                    exit();
+                }else if(!filter_var($this->email, FILTER_VALIDATE_EMAIL) === true){
+                    header("Location: newuser?error=invalidemail&uid=".$this->username);
+                    exit();
+                }else if (!preg_match("/^[a-zA-Z0-9]*$/", $this->username)){
+                    header("Location: newuser?error=invaliduid&email=".$email);
+                    exit();
                 }else{
-                    header("Location: newuser?error=sql");
+                    $rslt = $this->addUser();
+                    if ($rslt == TRUE){
+                        $this->redirect("gallery");
+                    }else{
+                        header("Location: newuser?error=sql");
+                    }
                 }
             }
         }
@@ -49,7 +57,6 @@
                     header("Location: newuser?error=emailexist&uid=".$this->username);
                 }else if ($checkuidmail === 'valid'){
                     $rslt = $newusermodel->addUserdata();
-                    $newusermodel->addProfile();
                     if ($rslt == TRUE){
                         return (TRUE);
                     }else{
@@ -62,7 +69,7 @@
         }
 
     public function checkPwdstrength($password){
-        if(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%])[0-9A-Za-z!@#$%]{8,50}$/', $password)) {
+        if(!preg_match('/^(?=.*\d)(?=.*[A-Za-z])(?=.*[!@#$%])[0-9A-Za-z!@#$%]{8,50}$/', $password) || strcmp($this->pwdrepeat, $this->pwd)) {
             return (FALSE);
         }else{
             return (TRUE);
--- Controllers/ProfileController.php
+++ Controllers/ProfileController.php
@@ -1,13 +1,50 @@
 <?php
     class ProfileController extends Controller{
-        public $link_pro;
-        function process($params){
-            $this->view = 'profile';
-            $profilemodel = new ProfileModel;
-            $this->link_pro = $profilemodel->profileImg();
-            $this->renderView();
+        public $pic_pro;
+        protected $email;
+        protected $username;
+        protected $nonhspwd;
+        protected $confirmed;
+
+        function process($parsedUrl){
+            $this->getData();
+            if ($parsedUrl[1] == 'modify'){
+                $this->view = 'modify';
+                $this->renderView();
+                if (isset($_POST['modify'])){
+                    $rslt = $this->modifyInfo();
+                    if ($rslt == 'pwderror'){
+                        header("Location: /profile/modify?error=pwdrpt"); 
+                    }
+                }
+            }else{
+                $this->view = 'profile';
+                $this->renderView();
+            }
         }
 
+        public function getData(){
+            $profilemodel = new ProfileModel;
+            $this->pic_pro = $profilemodel->profileImg();
+            $arr = $profilemodel->profileInfo();
+            if (isset($arr)){
+                $this->email = $arr['email'];
+                if ($arr['confirmed'] == 0){
+                    $this->confirmed = 'No';
+                }else{
+                    $this->confirmed = 'Yes';
+                }
+            }
+        }
+
+        public function modifyInfo(){
+            $this->email = $_POST['email'];
+            $this->username = $_POST['username'];
+            $this->nonhspwd = $_POST['pwd'];
+            if ($this->nonhspwd != $_POST['pwd-repeat']){
+                return ('pwderror');
+            }
+        }
         public function myGallery(){
             
         }

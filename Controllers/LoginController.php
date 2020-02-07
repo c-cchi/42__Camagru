89d74dedbb4adee90235360751515fc31ebceee7
Chia-han CHI <cchi@e1r4p12.42.fr>
2020-02-07 11:14:43 +0100

    solve login problem

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -1,59 +1,61 @@
 <?php
     class LoginController extends Controller{
 
-    public function process($login){
-        $this->view = 'Login';
-        if(isset($_POST['login'])){
-            $msg = $this->invoke();
-            if (isset($_SESSION['user'])){
-                $this->redirect('gallery');
-            }else{
-                echo "<h4>".$msg."</h4>";
-            }
-        }else if(isset($_POST['submit']) && $_POST['submit'] == "Logout"){
-            session_destroy();
-            $this->redirect('index');
-        }
-        if(isset($_SESSION['user'])){
-            include "views/Logout.phtml";
-        }else{
-            $this->renderView();
-        }
-    }
+        protected $username;
 
-    public function check_user($arr){
-        if (password_verify($_POST['passwd'], $arr['password']) == TRUE){
-            return (TRUE);
-        }else{
-            return (FALSE);
-        }
-    }
-
-    public function getlogin($sqlidata){
-        if (isset($_POST['login']) && isset($_POST['passwd'])){
-            if (!isset($sqlidata)){
-                session_destroy();
-                return ('usrnonexist');
-            }else{
-                $rslt = $this->check_user($sqlidata[0]);
-                if ($rslt == TRUE){
-                    $_SESSION['user'] = $sqlidata[0]['username'];
-                    $_SESSION['no'] = $sqlidata[0]['no'];
-                    return (TRUE);
-                }else{
-                    session_destroy();
-                    return ('incorrect password');
+        public function process($login){
+            $this->view = 'Login';
+            if(isset($_POST['uid'])){
+                $this->invoke();
+                if (isset($_SESSION['user'])){
+                    $this->redirect('gallery');
                 }
             }
+            if(isset($_POST['submit']) && $_POST['submit'] == "Logout"){
+                session_destroy();
+                $this->redirect('index');
+            }else if(isset($_SESSION['user'])){
+                include "views/Logout.phtml";
+            }else{
+                $this->renderView();
+            }
         }
-    }
 
-    public function invoke(){
-        $qry = "SELECT `username`,`password`,`no`,`email` FROM `users` WHERE `username`= :username OR `email`= :username";
-        $username = $_POST['login'];
-        $arr = array('username' => $username);
-        $sqlidata = Connection::getInstance()->runQuery($qry, $arr);
-        $rslt = $this->getlogin($sqlidata);
-        return ($rslt);
-    }
+        public function check_user($arr){
+            if (password_verify($_POST['passwd'], $arr['password']) == TRUE){
+                return (TRUE);
+            }else{
+                return (FALSE);
+            }
+        }
+
+        public function getlogin($sqlidata){
+            if (isset($_POST['uid']) && isset($_POST['passwd'])){
+                if (!isset($sqlidata)){
+                    session_destroy();
+                    header("Location: login?error=usrnotexist");
+                }else{
+                    $rslt = $this->check_user($sqlidata[0]);
+                    if ($rslt == TRUE){
+                        $_SESSION['user'] = $sqlidata[0]['username'];
+                        $_SESSION['no'] = $sqlidata[0]['no'];
+                        return (TRUE);
+                    }else{
+                        session_destroy();
+                        header("Location: login?error=incorrectpwd&uid=".$this->username);
+                    }
+                }
+            }else{
+                header("Location: login?error=emptyfields");
+            }
+        }
+
+        public function invoke(){
+            $qry = "SELECT `username`,`password`,`no`,`email` FROM `users` WHERE `username`= :username OR `email`= :username";
+            $username = $_POST['uid'];
+            $arr = array('username' => $username);
+            $sqlidata = Connection::getInstance()->runQuery($qry, $arr);
+            $rslt = $this->getlogin($sqlidata);
+            return ($rslt);
+        }
 }
